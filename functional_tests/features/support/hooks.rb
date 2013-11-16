b3464440c608604e0df8391fe9af90589446daea
Dmitriy <datonkushin@gmail.com>
2013-11-16 23:27:16 +0600

    Added report generation. To report attached screen shots of failed tests.

--- functional_tests/features/support/hooks.rb
+++ functional_tests/features/support/hooks.rb
@@ -4,6 +4,12 @@ Before do
   @browser = Selenium::WebDriver.for :firefox
 end
 
+After do |scenario|
+  if(scenario.failed?)
+    @browser.save_screenshot("reports/screenshots/#{scenario.__id__}.png")
+    embed("#{scenario.__id__}.png", "image/png", "SCREENSHOT")
+  end
+end
 
 After do
   @browser.close
